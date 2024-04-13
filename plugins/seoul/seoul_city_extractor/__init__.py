0924365b74c7fb6f90903c4a6e9f0086f91f503b
jackmappotion <jackmappotion@gmail.com>
2024-04-13 02:12:41 +0000

    [create] __init__

---
+++ plugins/seoul/seoul_city_extractor/__init__.py
@@ -0,0 +1,5 @@
+from seoul.seoul_city_extractor.seoul_city_extractor import SeoulCityExtractor
+
+from seoul.seoul_city_extractor.private import SeoulPrivate
+from seoul.seoul_city_extractor.assets import AREA_CDS
+
